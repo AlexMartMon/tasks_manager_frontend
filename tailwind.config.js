c1439dd2c46021727d09bc58e173b58f31957873
alejandro martin <alejandro12-11@hotmail.com>
2025-01-08 11:58:16 +0900

    talwind_config_error_fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+import formsPlugin from '@tailwindcss/forms';
+
 export default {
-  content: [ "./src/**/*.{js,jsx,ts,tsx}","./index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms')
+    formsPlugin
   ],
-}
-
+};
